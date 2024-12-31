cfcc60df414156df0c8004806e46628579b1c9d1
Mohammad.Madani <mohammad.madani74@gmail.com>
2024-12-31 20:05:53 +0330

    feat(subscription-plan): add is_promote field and update seeder for new subscription plans

--- prisma/seed/subscription-plans.seeder.ts
+++ prisma/seed/subscription-plans.seeder.ts
@@ -8,15 +8,25 @@ const prisma = new PrismaClient();
 /* -------------------------------------------------------------------------- */
 /*                                    SEED                                    */
 /* -------------------------------------------------------------------------- */
-const data = (): Prisma.SubscriptionPlanCreateInput => {
-  const data: Prisma.SubscriptionPlanCreateInput = {
-    duration: 30,
-    title: 'اشتراک ۳۰ روزه',
-    group: SubscriptionPlanGroup.PROPERTY,
-    price: 100_000,
-    price_with_discount: 90000,
-    is_active: true,
-  };
+const data = (): Prisma.SubscriptionPlanCreateInput[] => {
+  const data: Prisma.SubscriptionPlanCreateInput[] = [
+    {
+      duration: 30,
+      title: 'اشتراک ۳۰ روزه',
+      group: SubscriptionPlanGroup.PROPERTY,
+      price: 100_000,
+      price_with_discount: 90000,
+      is_active: true,
+    },
+    {
+      duration: 0,
+      title: 'نردبان',
+      group: SubscriptionPlanGroup.PROPERTY,
+      price: 100_000,
+      is_promote: true,
+      is_active: true,
+    },
+  ];
   return data;
 };
 
@@ -25,8 +35,10 @@ const data = (): Prisma.SubscriptionPlanCreateInput => {
 /* -------------------------------------------------------------------------- */
 export async function subscriptionPlanSeeder(): Promise<void> {
   console.time('✅ SUBSCRIPTION PLAN');
-  const item = await prisma.subscriptionPlan.findFirst({ where: { title: data().title } });
-  if (!item) await prisma.subscriptionPlan.create({ data: data() });
+  for (const e of data()) {
+    const item = await prisma.subscriptionPlan.findFirst({ where: { title: e.title } });
+    if (!item) await prisma.subscriptionPlan.create({ data: e });
+  }
 
   console.timeEnd('✅ SUBSCRIPTION PLAN');
 }
