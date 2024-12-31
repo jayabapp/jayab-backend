69e6317765a633722d7e5654549c2f810917daa9
Mohammad.Madani <mohammad.madani74@gmail.com>
2024-12-31 16:35:48 +0330

    [User]feat(property-option): update FindAllPropertyOptionUserDto to support array of groups

--- src/property-option/roles/user/dto/find-all.dto.ts
+++ src/property-option/roles/user/dto/find-all.dto.ts
@@ -1,11 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
-import { _IsEnum } from 'src/common/pipes/validator-translate.pipe';
+import { _ArrayNotEmpty, _IsEnum } from 'src/common/pipes/validator-translate.pipe';
 import { PropertyOptionGroup } from 'src/property-option/common/property-option-groups.type';
 
 export class FindAllPropertyOptionUserDto {
-  @ApiProperty({ required: true, enum: PropertyOptionGroup, default: PropertyOptionGroup.ACCESS })
-  @IsOptional()
-  @_IsEnum(PropertyOptionGroup)
-  group: PropertyOptionGroup;
+  @ApiProperty({
+    required: true,
+    example: ['PROPERTY_TYPE', 'ACCESS'],
+    type: String,
+    isArray: true,
+  })
+  // @_IsEnum(PropertyOptionGroup)
+  @_ArrayNotEmpty()
+  group: PropertyOptionGroup[];
 }
