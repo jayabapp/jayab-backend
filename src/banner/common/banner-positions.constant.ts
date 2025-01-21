268b2f20c773036a38d94005d76dd67fbc7ae1bc
Mohammad.Madani <mohammad.madani74@gmail.com>
2025-01-21 17:29:17 +0330

    [Admin]feat(banner): update banner positions and resize settings

--- src/banner/common/banner-positions.constant.ts
+++ src/banner/common/banner-positions.constant.ts
@@ -1,31 +1,30 @@
 import { EnumList } from 'src/common/interfaces/model-props.interface';
 
 export enum BannerPosition {
-  MAIN = 'MAIN',
-  MAIN_MIDDLE = 'MAIN_MIDDLE',
-  MAIN_BOTTOM = 'MAIN_BOTTOM',
-  POS1 = 'POS1',
-  // POS2 = 'POS2',
+  MAIN_1 = 'main_1',
+  MAIN_2 = 'main_2',
+  Advisor = 'advisor',
+  MAIN_SIDEBAR = 'main_sidebar',
 }
 export const BannerPositionList: EnumList[] = [
-  { id: BannerPosition.MAIN, title: 'اصلی - نسبت یک به چهار', hex: '#4f46e5', sub_title: 'بنر اصلی' },
+  { id: BannerPosition.MAIN_1, title: 'اصلی - نسبت یک به دو پنجم', hex: '#4f46e5', sub_title: 'بنر اصلی' },
   {
-    id: BannerPosition.MAIN_MIDDLE,
-    title: 'وسط صفحه اصلی - مربع',
+    id: BannerPosition.MAIN_2,
+    title: 'وسط صفحه اصلی',
     hex: '#eab308',
     sub_title: 'بنر وسط صفحه اصلی',
   },
   {
-    id: BannerPosition.MAIN_BOTTOM,
-    title: 'پایین صفحه اصلی - نسبت یک به چهار',
+    id: BannerPosition.Advisor,
+    title: 'مشاوران',
     hex: '#06b6d4',
-    sub_title: 'بنر پایین صفحه اصلی',
+    sub_title: '',
   },
   {
-    id: BannerPosition.POS1,
-    title: 'بنر چهارم',
+    id: BannerPosition.MAIN_SIDEBAR,
+    title: 'سایدبار',
     hex: '#E11D48',
-    sub_title: 'بنر چهارم',
+    sub_title: '',
   },
   // {
   //   id: BannerPosition.MAIN_BOTTOM,
