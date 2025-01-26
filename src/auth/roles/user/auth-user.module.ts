00b9d062aa8f9bb23c43a781eaac777b61746827
reza.madani <reza.maadani@gmail.com>
2025-01-26 14:16:46 +0330

    feat(auth): add advisor status to init api

--- src/auth/roles/user/auth-user.module.ts
+++ src/auth/roles/user/auth-user.module.ts
@@ -6,9 +6,10 @@ import { AttachmentModule } from 'src/attachment/attachment.module';
 import { AuthSharedService } from 'src/auth/auth-shared.service';
 import { FavoriteUserModule } from 'src/favorite/roles/user/user.module';
 import { BookmarkUserModule } from 'src/bookmark/roles/user/bookmark.module';
+import { ProfileUserModule } from 'src/profile/roles/user/profile-user.module';
 
 @Module({
-  imports: [SmsModule, AttachmentModule, FavoriteUserModule, BookmarkUserModule],
+  imports: [SmsModule, AttachmentModule, FavoriteUserModule, BookmarkUserModule, ProfileUserModule],
   controllers: [AuthUserController],
   providers: [AuthUserService, AuthSharedService],
 })
