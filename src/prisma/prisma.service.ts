cb52ded5323bcb42eb4d39ce68dfd92d2ee5333b
reza.madani <reza.maadani@gmail.com>
2026-04-15 18:53:12 +0330

    [Admin] fix(prisma): fix prisma count in soft delete

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -71,7 +71,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
             return query(args);
           },
           count({ model, args, query }) {
-            if (ModelsWithSoftDelete.includes(model as any)) {
+            if (ModelsWithSoftDelete.includes(model as any) && args?.where?.['deleted_at'] == undefined) {
               args.where = { ...args.where, deleted_at: null };
             } else delete args.where?.['deleted_at'];
             return query(args);
