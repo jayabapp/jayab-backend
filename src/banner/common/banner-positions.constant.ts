7b1590c36a7684cb9ae55f2d01eeff407196ae5e
reza.madani <reza.maadani@gmail.com>
2026-05-17 10:30:16 +0330

    chore(banner): change banner positions

--- src/banner/common/banner-positions.constant.ts
+++ src/banner/common/banner-positions.constant.ts
@@ -4,28 +4,21 @@ export enum BannerPosition {
   MAIN_1 = 'main_1',
   MAIN_2 = 'main_2',
   MAIN_3 = 'main_3',
-  ROOMS_1 = 'rooms_1',
   Advisor = 'advisor',
 }
 export const BannerPositionList: EnumList[] = [
   { id: BannerPosition.MAIN_1, title: 'بالای صفحه اصلی', hex: '#4f46e5', sub_title: 'بنر اصلی' },
   {
     id: BannerPosition.MAIN_2,
-    title: 'بین آگهی ها - صفحه اصلی',
+    title: 'تبلیغات بین آگهی',
     hex: '#eab308',
-    sub_title: 'بین آگهی ها - صفحه اصلی',
+    sub_title: 'تبلیغات بین آگهی',
   },
   {
     id: BannerPosition.MAIN_3,
     title: 'پایین صفحه اصلی',
     hex: '#1af308',
     sub_title: 'پایین صفحه اصلی',
-  },
-  {
-    id: BannerPosition.ROOMS_1,
-    title: 'بین آگهی ها - عادی',
-    hex: '#6366F1',
-    sub_title: 'بین آگهی ها - عادی',
   },
   {
     id: BannerPosition.Advisor,
