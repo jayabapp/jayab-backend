470518c75048d2d6a47292d4cb0f01f40f0d378a
reza.madani <reza.maadani@gmail.com>
2026-07-20 10:32:46 +0330

    feat(client): check owner property status

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PropertyStatuses } from 'src/property/common/types/property-status.type';
 import { CheckMobileIsExistClientDto } from './dto/check-mobile.dto';
 
 @Injectable()
 export class ClientService {
   constructor(private readonly db: PrismaService) {}
 
-  async checkMobileIsExist(dto: CheckMobileIsExistClientDto): Promise<any> {
-    const isExist = await this.db.user.findUnique({ where: { mobile_number: dto.mobile_number } });
-    return Boolean(isExist);
+  async checkMobileIsExist(dto: CheckMobileIsExistClientDto): Promise<boolean> {
+    const owner = await this.db.owner.findFirst({
+      where: {
+        user: { mobile_number: dto.mobile_number },
+        properties: {
+          some: {
+            status: {
+              in: [PropertyStatuses.WAITING, PropertyStatuses.PUBLISHED],
+            },
+          },
+        },
+      },
+      select: { id: true },
+    });
+
+    const isExist = Boolean(owner);
+    return isExist;
   }
 }
