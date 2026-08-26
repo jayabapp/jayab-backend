ff1c2ed83e87a746287d4346d8bbe5315f2980d7
Hasan Moosavi <h.mousavi910@gmail.com>
2026-08-26 13:52:40 +0330

    refactor: clean notifications

--- src/notification/roles/user/user.controller.ts
+++ src/notification/roles/user/user.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Query, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { UserJwtGuard } from 'src/auth/guards/jwt/user-jwt.guard';
-import { NotificationUserService } from './user.service';
-import { SuccessResponseArgs } from 'src/common/interceptors/transform.interceptor';
-import { FindAllNotificationUserDto } from './dto/find-all.dto';
-import { UserRole } from 'src/common/interfaces/role.enum';
-import { UserType, RequestType } from 'src/common/interfaces/user.interface';
-import { NotificationSharedService } from '../shared/shared.service';
-import { FindAllNotificationSharedDto } from '../shared/dto/find-all.dto';
 import { USER_NOTIFICATION_ROUTE_GROUP } from 'src/notification/common/route-group.constant';
+import { FindAllNotificationSharedDto } from '../shared/dto/find-all.dto';
+import { NotificationSharedService } from '../shared/shared.service';
+import { SuccessResponseArgs } from 'src/common/interceptors/transform.interceptor';
+import { UserJwtGuard } from 'src/auth/guards/jwt/user-jwt.guard';
+import { RequestType } from 'src/common/interfaces/user.interface';
+import { UserRole } from 'src/common/interfaces/role.enum';
 
 @ApiTags('Notification - USER')
 @UseGuards(UserJwtGuard)
@@ -37,19 +35,6 @@ export class NotificationUserController {
       user.notification_read_at,
       UserRole.USER,
     );
-
     return { result };
   }
-
-  // @ApiOperation({ summary: 'Find One', description: '' })
-  // @Get(':notifId')
-  // async findOne(
-  //   @Req() req: UserType,
-  //   @Param('notifId', ParseIntPipe) notifId: number,
-  // ): Promise<SuccessResponseArgs> {
-  //   const user = req.user;
-  //   const result = await this.baseUserService.findOne(user.id, UserRole.USER, notifId);
-
-  //   return { result };
-  // }
 }
